98cf5c88cab351ff2107ee12b7201d12c5ac5138
kshanu12 <kumarshanu38370@gmail.com>
2024-01-03 01:27:05 +0530

    fixed issue of total coins

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ var total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 io.on("connection", (socket) => {
   console.log("A User Connected");
+
+  socket.emit("refresh_changes", total);
+
+  socket.on("new_game", () => {
+    total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  })
+
+  socket.on("generate_result", () => {
+    const cardWon = Math.floor((Math.random() * (12)) + 0);
+    io.emit("card_won",cardWon);
+  });
+
   socket.on("new_bet", (index, newAmount) => {
     total[index] += newAmount;
     console.log("inside new_bet");
